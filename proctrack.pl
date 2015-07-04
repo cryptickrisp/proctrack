71a50f2a2b59a2383c65dbb0da7638cfb44e1d29
Kris Popendorf <krisp@krisp.jp>
2015-07-04 21:25:14 +0900

    relicense

--- proctrack.pl
+++ proctrack.pl
@@ -1,6 +1,12 @@
 #!/usr/bin/perl
-
-#by Kris Popendorf <krisp@dna.bio.keio.ac.jp>
+#
+# ----------------------------------------------------------------------------
+# "THE BEER-WARE LICENSE" (Revision 42):
+# <krisp@krisp.jp> wrote this file.  As long as you retain this notice you
+# can do whatever you want with this stuff. If we meet some day, and you think
+# this stuff is worth it, you can buy me a beer in return.   Kris Popendorf
+# ----------------------------------------------------------------------------
+#
 
 use strict;
 use Getopt::Long;
